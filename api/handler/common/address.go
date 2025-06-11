c8f2d6ac882b83d4e264721d6c821d65869b964a
suha jin <89185836+djm07073@users.noreply.github.com>
2025-06-11 13:53:32 +0900

    Update address.go

--- api/handler/common/address.go
+++ api/handler/common/address.go
@@ -8,14 +8,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func EnsureLowerHexAddress(addr string) (string, error) {
-	addr = strings.ToLower(addr)
-	if _, err := hex.DecodeString(addr); err != nil {
-		return "", fmt.Errorf("invalid hex address: %s", addr)
-	}
-	return addr, nil
-}
-
 func AccAddressFromString(addrStr string) (sdk.AccAddress, error) {
 	if !strings.HasPrefix(addrStr, "0x") {
 		return sdk.AccAddressFromBech32(addrStr)
