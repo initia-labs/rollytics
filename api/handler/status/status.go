63e58d90f753877ad1b34b68e563eaa29489c9f0
suha jin <89185836+djm07073@users.noreply.github.com>
2025-07-24 16:15:57 +0900

    feat(api): return status of last internal tx height

--- api/handler/status/status.go
+++ api/handler/status/status.go
@@ -16,20 +16,33 @@ import (
 // @Success 200 {object} StatusResponse
 // @Router /status [get]
 func (h *StatusHandler) GetStatus(c *fiber.Ctx) error {
-	var lastBlock types.CollectedBlock
+	var (
+		lastBlock         types.CollectedBlock
+		lastEvmInternalTx types.CollectedEvmInternalTx
+	)
+
 	if err := h.GetDatabase().
 		Model(&types.CollectedBlock{}).
 		Where("block.chain_id = ?", h.GetChainId()).
 		Order("height DESC").
-		Limit(1).
 		First(&lastBlock).Error; err != nil {
 		return fiber.NewError(fiber.StatusInternalServerError, err.Error())
 	}
 
+	if h.GetChainConfig().VmType == types.EVM && h.GetConfig().InternalTxEnabled() {
+		if err := h.GetDatabase().
+			Model(&types.CollectedEvmInternalTx{}).
+			Order("height DESC").
+			First(&lastEvmInternalTx).Error; err != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, err.Error())
+		}
+	}
+
 	return c.JSON(&StatusResponse{
-		Version:    config.Version,
-		CommitHash: config.CommitHash,
-		ChainId:    h.GetChainId(),
-		Height:     lastBlock.Height,
+		Version:          config.Version,
+		CommitHash:       config.CommitHash,
+		ChainId:          h.GetChainId(),
+		Height:           lastBlock.Height,
+		InternalTxHeight: lastEvmInternalTx.Height,
 	})
 }
