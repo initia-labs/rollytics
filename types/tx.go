a122a245cf03513c1a7b42722456825e94ae09b2
suha jin <89185836+djm07073@users.noreply.github.com>
2025-08-19 16:58:51 +0900

    chore: response to string in big number thing

--- types/tx.go
+++ types/tx.go
@@ -9,15 +9,15 @@ import (
 
 type Tx struct {
 	TxHash    string              `json:"txhash"`
-	Height    int64               `json:"height"`
+	Height    int64               `json:"height,string"`
 	Codespace string              `json:"codespace"`
 	Code      uint32              `json:"code"`
 	Data      string              `json:"data"`
 	RawLog    string              `json:"raw_log"`
 	Logs      sdk.ABCIMessageLogs `json:"logs"`
 	Info      string              `json:"info"`
-	GasWanted int64               `json:"gas_wanted"`
-	GasUsed   int64               `json:"gas_used"`
+	GasWanted int64               `json:"gas_wanted,string"`
+	GasUsed   int64               `json:"gas_used,string"`
 	Tx        json.RawMessage     `json:"tx"`
 	Timestamp time.Time           `json:"timestamp"`
 	Events    json.RawMessage     `json:"events"`
