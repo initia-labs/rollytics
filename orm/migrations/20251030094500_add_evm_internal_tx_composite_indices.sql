38eb26fdf0976bccc912aa3ab53a441ffa293966
Nattharat Kondanna Wiriyakulnan <nattharat.wi@gmail.com>
2025-10-31 15:34:26 +0700

    chore(migration): add CONCURRENTLY option to composite index creation for evm_internal_tx (#75)

--- orm/migrations/20251030094500_add_evm_internal_tx_composite_indices.sql
+++ orm/migrations/20251030094500_add_evm_internal_tx_composite_indices.sql
@@ -1,10 +1,11 @@
+-- atlas:txmode none
 
 -- Create composite indices for "evm_internal_tx" matching gorm tags in types/table.go
 
 -- (height, sequence DESC)
-CREATE INDEX "evm_internal_tx_height_sequence_desc"
+CREATE INDEX CONCURRENTLY "evm_internal_tx_height_sequence_desc"
 ON "public"."evm_internal_tx" ("height", "sequence" DESC);
 
 -- (hash_id, sequence DESC)
-CREATE INDEX "evm_internal_tx_hash_sequence_desc"
+CREATE INDEX CONCURRENTLY "evm_internal_tx_hash_sequence_desc"
 ON "public"."evm_internal_tx" ("hash_id", "sequence" DESC);
